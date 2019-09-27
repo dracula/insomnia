23c216287485e7054d8bad82ffcec8267ceb0dc3
Matheus <matheus12.kp@gmail.com>
2019-09-27 15:36:08 -0300

    Initial commit

---
+++ themes/dracula.js
@@ -0,0 +1,60 @@
+module.exports = {
+	name: 'dracula',
+	displayName: 'Dracula',
+	theme: {
+		background: {
+			default: '#282a36',
+			success: '#50fa7b',
+			notice: '#f1fa8c',
+			warning: '#ffb86c',
+			danger: '#ff5555',
+			surprise: '#bd93f9',
+			info: '#8be9fd'
+		},
+		foreground: {
+			default: '#f8f8f2',
+			success: '#f8f8f2',
+			notice: '#f8f8f2',
+			warning: '#f8f8f2',
+			danger: '#f8f8f2',
+			surprise: '#f8f8f2',
+			info: '#f8f8f2'
+		},
+		highlight: {
+			default: 'rgba(98, 114, 164, .5)',
+			xxs: 'rgba(98, 114, 164, 0.05)',
+			xs: 'rgba(98, 114, 164, 0.1)',
+			sm: 'rgba(98, 114, 164, 0.2)',
+			md: 'rgba(98, 114, 164, 0.4)',
+			lg: 'rgba(98, 114, 164, 0.6)',
+			xl: 'rgba(98, 114, 164, 0.8)'
+		},
+		styles: {
+			sidebar: {
+				background: {
+					default: '#1d1f27'
+				}
+			},
+			dialog: {
+				background: {
+					default: '#282a36'
+				}
+			},
+			paneHeader: {
+				background: {
+					success: '#50fa7b',
+					notice: '#f1fa8c',
+					warning: '#ffb86c',
+					danger: '#ff5555',
+					surprise: '#bd93f9',
+					info: '#8be9fd'
+				}
+			},
+			transparentOverlay: {
+				background: {
+					default: 'rgba(40, 42, 54, 0.5)'
+				}
+			}
+		}
+	}
+}
